429a9c40104dab6e20b8026844e2c31af0b0a985
unknown <wbc2504@gmail.com>
2024-03-17 18:13:31 -0500

    change hello world by will

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿using System.Reflection.Metadata.Ecma335;
+namespace HelloWorld{
+    class Program{
+        
+        static void Main(){
+            var Name="Jhon";
+            Name="1Jhon";
+            Console.WriteLine($"Hello world! {Name}");
+            Console.WriteLine();
+        } 
+    }
+}
